104dc706ff9b5f72dcf529123df401d775113a09
Jean-Nicolas Artaud <jeannicolasartaud@gmail.com>
2016-05-02 17:13:54 +0200

    Fulfill the Account, adding what has been designed
    Add properties games, wins and elo
    Add private webSocket
    Add function disconnected

--- Core/account.cpp
+++ Core/account.cpp
@@ -1,18 +1,67 @@
 #include "account.h"
 
 #include <QWebSocket>
+#include <QMetaProperty>
+#include <QDebug>
 
 #include "xmltools.h"
 
 Account::Account(QObject *parent) :
-	QObject(parent)
+	QObject(parent), m_login(), m_password(), m_iconUrl(), m_wins(0), m_games(0), m_elo(2000), m_client(NULL)
 {
 }
 
 QString Account::xmlState()
 {
+	QString fullXml;
 	XmlTools tool;
-	return tool.xmlFromValue("Login",m_login);
+
+	for (int i = 1; i < metaObject()->propertyCount(); ++i) {
+		qDebug() << metaObject()->property(i).name();
+		qDebug() << metaObject()->property(i).read(this).toString();
+	}
+
+	return fullXml;
+}
+
+void Account::addGameResult(bool win, int adversaryElo)
+{
+	m_games++;
+	if (win) {
+		m_wins++;
+		// Compute the elo in win case
+		int elo;
+		if (m_elo == adversaryElo) {
+			elo = 5;
+		} else if (adversaryElo > m_elo) {
+			elo = (0.2 * (adversaryElo - m_elo));
+		} else {
+			elo = (10 / (m_elo - adversaryElo));
+		}
+		m_elo += elo;
+	} else {
+		// Compute the elo in lose case
+		int elo;
+		if (m_elo == adversaryElo) {
+			elo = 5;
+		} else if (adversaryElo > m_elo) {
+			elo = (10 / (m_elo - adversaryElo));
+		} else {
+			elo = (0.2 * (adversaryElo - m_elo));
+		}
+
+		m_elo -= elo;
+	}
+	xmlState();
+}
+
+void Account::disconnected()
+{
+	if (m_client != NULL) {
+		return;
+	}
+	delete m_client;
+	m_client = NULL;
 }
 
 QString Account::login() const
@@ -43,15 +92,55 @@ void Account::setPassword(const QString &password)
 
 QString Account::iconUrl() const
 {
-    return m_iconUrl;
+	return m_iconUrl;
 }
 
 void Account::setIconUrl(const QString &email)
 {
-    if (email != m_iconUrl) {
-        m_iconUrl = email;
-        emit iconUrlChanged();
+	if (email != m_iconUrl) {
+		m_iconUrl = email;
+		emit iconUrlChanged();
 	}
+}
+
+int Account::wins() const
+{
+	return m_wins;
+}
+
+void Account::setWins(int wins)
+{
+	m_wins = wins;
+}
+
+int Account::games() const
+{
+	return m_games;
+}
+
+void Account::setGames(int games)
+{
+	m_games = games;
+}
+
+int Account::elo() const
+{
+	return m_elo;
+}
+
+void Account::setElo(int elo)
+{
+	m_elo = elo;
+}
+
+QWebSocket *Account::client() const
+{
+	return m_client;
+}
+
+void Account::setClient(QWebSocket *client)
+{
+	m_client = client;
 }
 
 
--- Core/account.h
+++ Core/account.h
@@ -12,10 +12,15 @@ class Account : public QObject
 	Q_PROPERTY(QString login READ login WRITE setLogin NOTIFY loginChanged)
 	Q_PROPERTY(QString password READ password WRITE setPassword NOTIFY passwordChanged)
     Q_PROPERTY(QString iconUrl READ iconUrl WRITE setIconUrl NOTIFY iconUrlChanged)
+	Q_PROPERTY(int wins READ wins WRITE setWins)
+	Q_PROPERTY(int games READ games WRITE setGames)
+	Q_PROPERTY(int elo READ elo WRITE setElo)
 
 public:
 	explicit Account(QObject *parent = 0);
 	QString xmlState();
+	void addGameResult(bool win, int adversaryElo);
+	void disconnected();
 
 	QString login() const;
 	void setLogin(const QString &login);
@@ -26,10 +31,22 @@ public:
     QString iconUrl() const;
     void setIconUrl(const QString &iconUrl);
 
+	int wins() const;
+	void setWins(int wins);
+
+	int games() const;
+	void setGames(int games);
+
+	int elo() const;
+	void setElo(int elo);
+
+	QWebSocket *client() const;
+	void setClient(QWebSocket *client);
+
 signals:
 	void loginChanged();
 	void passwordChanged();
-    void iconUrlChanged();
+	void iconUrlChanged();
 
 public slots:
 
@@ -37,8 +54,10 @@ private:
 	QString m_login;
 	QString m_password;
     QString m_iconUrl;
-	QWebSocket *m_clients;
-
+	int		m_wins;
+	int		m_games;
+	int		m_elo;
+	QWebSocket *m_client;
 };
 
 #endif // ACCOUNT_H
